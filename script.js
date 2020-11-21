d26791e533992a583ad38eafc9bfd5d0e4e18238
Victoria Lee <victoriacmlee@gmail.com>
2020-11-20 21:54:55 -0500

    format code for edit

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 // show/hide contacts icons toggle
 const hereEl = document.querySelector("#hover-here")
 const iconContainerEl = document.querySelector("#icon-container")
+
 function showContacts() {
     console.log("works")
     iconContainerEl.classList.toggle("hide")
